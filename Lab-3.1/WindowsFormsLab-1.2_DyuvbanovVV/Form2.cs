f56d6c500675d9a182c6587e9b29a40b80f64288
VitaliyD <TheWalkSideMail@gmail.com>
2022-03-23 21:34:52 +0300

    init: add Lab-3.1

---
+++ Lab-3.1/WindowsFormsLab-1.2_DyuvbanovVV/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab_1._2_DyuvbanovVV
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form1 f)
+        {
+            int num3;
+            num3 = f.num3;
+            InitializeComponent();
+            textBox1.Text = num3.ToString();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
